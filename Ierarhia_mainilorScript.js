7eb746e5be4b111e244b69ee94d81bd40680ed20
cristian990 <cristi.096utd@gmail.com>
2019-12-16 13:28:19 +0200

    Add files via upload

---
+++ Ierarhia_mainilorScript.js
@@ -0,0 +1,96 @@
+
+function myFunction1() {
+    var x = document.getElementById("Hand1");
+    if (x.style.display == "block") 
+        x.style.display = "none";
+     else 
+        x.style.display = "block";
+    
+}
+
+
+function myFunction2() {
+    var x = document.getElementById("Hand2");
+    if (x.style.display == "block") 
+        x.style.display = "none";
+     else 
+        x.style.display = "block";
+    
+}
+
+function myFunction3() {
+    var x = document.getElementById("Hand3");
+    if (x.style.display == "block") 
+        x.style.display = "none";
+     else 
+        x.style.display = "block";
+    
+}
+
+function myFunction4() {
+    var x = document.getElementById("Hand4");
+    if (x.style.display == "block") 
+        x.style.display = "none";
+     else 
+        x.style.display = "block";
+    
+}
+
+function myFunction5() {
+    var x = document.getElementById("Hand5");
+    if (x.style.display == "block") 
+        x.style.display = "none";
+     else 
+        x.style.display = "block";
+    
+}
+
+function myFunction6() {
+    var x = document.getElementById("Hand6");
+    if (x.style.display == "block") 
+        x.style.display = "none";
+     else 
+        x.style.display = "block";
+    
+}
+
+function myFunction7() {
+    var x = document.getElementById("Hand7");
+    if (x.style.display == "block") 
+        x.style.display = "none";
+     else 
+        x.style.display = "block";
+    
+}
+
+function myFunction8() {
+    var x = document.getElementById("Hand8");
+    if (x.style.display == "block") 
+        x.style.display = "none";
+     else 
+        x.style.display = "block";
+    
+}
+
+function myFunction9() {
+    var x = document.getElementById("Hand9");
+    if (x.style.display == "block") 
+        x.style.display = "none";
+     else 
+        x.style.display = "block";
+    
+}
+
+function myFunction10() {
+    var x = document.getElementById("Hand10");
+    if (x.style.display == "block") 
+        x.style.display = "none";
+     else 
+        x.style.display = "block";
+    
+}
+
+
+
+
+
